b437a6dd33f8810edf7bc4eaf202ec36329f0be7
Geetam <geetambinani6@gmail.com>
2024-08-11 19:39:24 +0530

    user api done

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
- * <--------------- health check endpoint ------------------>
+//  * <--------------- health check endpoint ------------------>
 app.get("/health", (req:Request, res:Response) => {
   res.send({
     message:'health okay'
